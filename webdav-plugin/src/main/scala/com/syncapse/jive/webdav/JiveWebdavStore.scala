a922a772c1adb9efdd03a290b3f4657e30c220ac
A.J. Wright <ayax79@gmail.com>
2010-08-26 10:41:48 -0700

    Added a bunch of logging.

--- webdav-plugin/src/main/scala/com/syncapse/jive/webdav/JiveWebdavStore.scala
+++ webdav-plugin/src/main/scala/com/syncapse/jive/webdav/JiveWebdavStore.scala
@@ -5,46 +5,63 @@ import java.io.InputStream
 import java.lang.String
 import net.sf.webdav.{ITransaction, IWebdavStore}
 import com.jivesoftware.community.{DocumentManager, CommunityManager}
+import com.syncapse.jive.Loggable
 
-class JiveWebdavStore(communityMgr: CommunityManager, documentManager: DocumentManager) extends IWebdavStore {
-
-
+class JiveWebdavStore(communityMgr: CommunityManager, documentManager: DocumentManager) extends IWebdavStore with Loggable {
   def getStoredObject(transaction: ITransaction, uri: String) = {
-
-
-
-    
+    logger.info("JiveWebStore getStoredObject: " + uri)
+    null
   }
 
   def removeObject(transaction: ITransaction, uri: String) = {
-
-
-
+    logger.info("JiveWebStore removeObject: " + uri)
+    null
   }
 
-  def getResourceLength(transaction: ITransaction, path: String) = 0L
+  def getResourceLength(transaction: ITransaction, path: String) = {
+    logger.info("JiveWebStore getResourceLength: " + path)
+    0L
+  }
 
   def getChildrenNames(transaction: ITransaction, folderUri: String) = {
-    communityMgr.getCommunity
-
-
-
-
+    logger.info("JiveWebStore getChildrenNames: " + folderUri)
+    null
   }
 
-  def setResourceContent(transaction: ITransaction, resourceUri: String, content: InputStream, contentType: String, characterEncoding: String) = 0L
+  def setResourceContent(transaction: ITransaction, resourceUri: String, content: InputStream, contentType: String, characterEncoding: String) = {
+    logger.info("JiveWebStore setResourceContent: " + resourceUri)
+    0L
+  }
 
-  def getResourceContent(transaction: ITransaction, resourceUri: String) = null
+  def getResourceContent(transaction: ITransaction, resourceUri: String) = {
+    logger.info("JiveWebStore getResourceContent: " + resourceUri)
+    null
+  }
 
-  def createResource(transaction: ITransaction, resourceUri: String) = {}
+  def createResource(transaction: ITransaction, resourceUri: String) = {
+    logger.info("JiveWebStore createResource: " + resourceUri)
+    null
+  }
 
-  def createFolder(transaction: ITransaction, folderUri: String) = {}
+  def createFolder(transaction: ITransaction, folderUri: String) = {
+    logger.info("JiveWebStore createFolder: " + folderUri)
+    null
+  }
 
-  def rollback(transaction: ITransaction) = {}
+  def rollback(transaction: ITransaction) = {
+    logger.info("rollback called")
+  }
 
-  def commit(transaction: ITransaction) = {}
+  def commit(transaction: ITransaction) = {
+    logger.info("commit called")
+  }
 
-  def checkAuthentication(transaction: ITransaction) = {}
+  def checkAuthentication(transaction: ITransaction) = {
+    logger.info("checkAuthentication called")
+  }
 
-  def begin(principal: Principal) = null
+  def begin(principal: Principal) = {
+    logger.info("begin called")
+    null
+  }
 }
