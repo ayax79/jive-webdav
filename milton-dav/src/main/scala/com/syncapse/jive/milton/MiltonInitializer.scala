93ea1b21cf3e49a1969d7e06cf92ead42c415ba2
A.J. Wright <ayax79@gmail.com>
2010-12-15 15:43:44 -0800

    work on trying to configure a filter directly into the plugin, fails because of not being available to the classloader.

--- milton-dav/src/main/scala/com/syncapse/jive/milton/MiltonInitializer.scala
+++ milton-dav/src/main/scala/com/syncapse/jive/milton/MiltonInitializer.scala
@@ -3,26 +3,28 @@ package com.syncapse.jive.milton
 import com.jivesoftware.community.lifecycle.{ApplicationState, ApplicationStateChangeEvent}
 import com.syncapse.jive.servlet.{AppServerUtils, ServletContextComponent}
 import com.syncapse.jive.event.ApplicationStateChangeListener
+import org.springframework.aop.SpringProxy
+import com.syncapse.jive.Loggable
 
 object MiltonInitializer {
   protected[milton] var webdavManager: WebdavManager = null
 }
 
-class MiltonInitializerextends extends ApplicationStateChangeListener with ServletContextComponent with WebdavManagerComponent {
+class MiltonInitializer extends ApplicationStateChangeListener with SpringProxy with ServletContextComponent with WebdavManagerComponent with Loggable {
 
   def handle(e: ApplicationStateChangeEvent) = e.getNewState match {
     case ApplicationState.RUNNING => initialize
     case ApplicationState.SHUTDOWN => shutdown
+    case _ => logger.debug("ignoring event" + e.getNewState)
   }
 
   protected def initialize = {
     MiltonInitializer.webdavManager = webdavManger
-    AppServerUtils.registerFilter(servletContext, "webdav", classOf[MiltonFilter].getName, "/webdav")
+    AppServerUtils.registerFilter(servletContext, "webdav", classOf[MiltonFilter].getName, "/webdav*")
   }
 
   protected def shutdown = {
     MiltonInitializer.webdavManager = null
   }
-
 
 }
