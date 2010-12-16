1a039dc39e8bf14e31b308e3856172d56e7d53ab
A.J. Wright <ayax79@gmail.com>
2010-12-15 16:30:45 -0800

    switched to putting the filter in the acegi stack. overrode the acegi stack will need to address this at somepoint.

--- milton-dav/src/main/scala/com/syncapse/jive/milton/MiltonFilter.scala
+++ milton-dav/src/main/scala/com/syncapse/jive/milton/MiltonFilter.scala
@@ -4,12 +4,10 @@ import javax.servlet._
 import com.bradmcevoy.http.{ServletRequest => WDRequest, ServletResponse => WDResponse}
 import http.{HttpServletResponse, HttpServletRequest}
 
-class MiltonFilter extends Filter {
+class MiltonFilter extends Filter with WebdavManagerComponent {
 
-  def doFilter(p1: ServletRequest, p2: ServletResponse, p3: FilterChain) = {
-    MiltonInitializer.webdavManager.process(new WDRequest(p1.asInstanceOf[HttpServletRequest]),
-      new WDResponse(p2.asInstanceOf[HttpServletResponse]))
-  }
+  def doFilter(p1: ServletRequest, p2: ServletResponse, p3: FilterChain) =
+    webdavManger.process(new WDRequest(p1.asInstanceOf[HttpServletRequest]), new WDResponse(p2.asInstanceOf[HttpServletResponse]))
 
   def init(p1: FilterConfig) = {}
 
